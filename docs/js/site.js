2f016e1af9c78deedbf063e438d6281762ab9d2f
Nicholas Lee <csudcy@gmail.com>
2018-04-28 12:12:29 +0100

    Add settings for 8 cards, jquery locally for offline dev, started image storage

--- docs/js/site.js
+++ docs/js/site.js
@@ -1,42 +1,120 @@
 $(document).ready(function() {
-  let IMAGES = [];
+  'use strict';
+  let DB;
 
-  function add_image() {
-    // TODO
+  function _add_promise_handlers_for_request(request, resolve, reject) {
+    request.onsuccess = function() {
+      resolve(request.result);
+    };
+
+    request.onerror = function() {
+      console.log('Error!');
+      console.log(request);
+      console.log(this);
+      console.log(err);
+      reject(request);
+    };
   }
 
-  function image_drag_start() {
-    $('#drag_overlay').show();
+  function init_database() {
+    DB = new Promise(function(resolve, reject) {
+      let open = indexedDB.open('dgen', 1);
+
+      // Create the schema
+      open.onupgradeneeded = function() {
+        let upgradeDb = open.result;
+        if (!upgradeDb.objectStoreNames.contains('images')) {
+          upgradeDb.createObjectStore('images', {keyPath: 'id', autoIncrement: true});
+        }
+      };
+
+      _add_promise_handlers_for_request(open, resolve, reject);
+    });
   }
 
-  function image_drag_end() {
-    $('#drag_overlay').hide();
+  function db_select(table) {
+    return new Promise(function(resolve, reject) {
+      DB.then(function(db) {
+        var tx = db.transaction(table, 'readonly');
+        var store = tx.objectStore(table);
+        var request = store.getAll();
+        _add_promise_handlers_for_request(request, resolve, reject);
+      });
+    });
   }
+
+  function db_insert(table, value) {
+    return new Promise(function(resolve, reject) {
+      DB.then(function(db) {
+        var tx = db.transaction(table, 'readwrite');
+        var store = tx.objectStore(table);
+        var request = store.put(value);
+        _add_promise_handlers_for_request(request, resolve, reject);
+      });
+    });
+  }
+
+  function db_update(table, value, key) {
+    return new Promise(function(resolve, reject) {
+      DB.then(function(db) {
+        var tx = db.transaction(table, 'readwrite');
+        var store = tx.objectStore(table);
+        var request = store.put(value, key);
+        _add_promise_handlers_for_request(request, resolve, reject);
+      });
+    });
+  }
+
+  /////////////////////////////
+  // Image Management
+  /////////////////////////////
+
+  function add_image(file) {
+    console.log(file);
+    db_insert('images', file);
+    // $('<img>').
+  }
+
+  function load_images() {
+    db_select('images').then(function(images) {
+      console.log(images);
+    });
+  }
+
+
+  /////////////////////////////
+  // Init functions
+  /////////////////////////////
 
   function bind_drag_handlers() {
     // Add drag/drop handling
     $('html').on('dragover', function(event) {
       event.preventDefault();
       event.stopPropagation();
-      image_drag_start();
+      $('#drag_overlay').show();
     });
 
     $('#drag_overlay').on('dragleave', function(event) {
       event.preventDefault();
       event.stopPropagation();
-      image_drag_end();
+      $('#drag_overlay').hide();
     });
 
     $('#drag_overlay').on('drop', function(event) {
       event.preventDefault();
       event.stopPropagation();
-      image_drag_end();
+      $('#drag_overlay').hide();
       $.each(event.originalEvent.dataTransfer.files, function(index, file) {
-        console.log(file);
+        add_image(file);
       });
     });
   }
 
-  // Init
+  /////////////////////////////
+  // Init everything
+  /////////////////////////////
+
+  init_database();
+  load_images();
   bind_drag_handlers();
 });
