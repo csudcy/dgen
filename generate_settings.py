f822f1d0845e6bab6cf552894cef03a3bee48e20
Nicholas Lee <csudcy@gmail.com>
2024-04-05 16:54:27 +0100

    Improve generate settings output

--- generate_settings.py
+++ generate_settings.py
@@ -1,3 +1,4 @@
+import datetime
 import itertools
 import math
 import pathlib
@@ -39,6 +40,7 @@ def n_choose_k(n: int, k: int) -> int:
 
 # Adapted from https://codegolf.stackexchange.com/questions/101229/dobble-spotit-card-generator
 def make_combos(settings: Settings) -> list[set[int]]:
+  print()
   print(f'Making combos for {settings.items_per_card} items_per_card...')
 
   items = range(settings.items_required)
@@ -46,14 +48,25 @@ def make_combos(settings: Settings) -> list[set[int]]:
 
   used_combos = []
   check_count = 0
-  next_output = 0
+  start_time = time.time()
+  next_output = start_time + 1
+  has_output_status = False
   for combo in itertools.combinations(items, settings.items_per_card):
     check_count += 1
     if time.time() > next_output:
       next_output = time.time() + 1
+      diff_time = time.time() - start_time
+      diff_time_td = datetime.timedelta(seconds=int(diff_time))
+      combinations_per_second = check_count / diff_time
+      remaining_time = (combination_count - check_count) / combinations_per_second
+      remaining_time_td = datetime.timedelta(seconds=int(remaining_time))
       pc_done = float(check_count) / float(combination_count) * 100.0
-      pc_found = float(len(used_combos)) / float(settings.items_required) * 100.0
-      print(f'  Found {pc_found:.2f}% ({len(used_combos)} / {settings.items_required}), checked {pc_done:.2f}% ({check_count} / {combination_count})...')
+      print(
+          f'  {diff_time_td}: Found {len(used_combos)} of {settings.items_required} '
+          f'({pc_done:,.2f}% checked, {combinations_per_second:,.2f}/s, {remaining_time_td} remaining) ...',
+          end='\r', flush=True)
+      has_output_status = True
+
     potential_combo = set(combo)
     # Work out if this combination is a valid combination
     # Valid combinations share exactly 1 item with every other card
@@ -64,12 +77,20 @@ def make_combos(settings: Settings) -> list[set[int]]:
     if valid:
       used_combos.append(potential_combo)
 
+  if has_output_status:
+    # Move to next line
+    print()
+
   if check_count != combination_count:
     print(f'***** WARNING: Expected {combination_count} combinations but checked {check_count} !')
   if len(used_combos) != settings.items_required:
     print(f'***** WARNING: Expected {settings.items_required} valid combinations but found {len(used_combos)} !')
 
-  print(f'Found: {len(used_combos)} valid combinations (expected {settings.items_required}) out of {check_count} total!')
+  diff_time = time.time() - start_time
+  diff_time_td = datetime.timedelta(seconds=int(diff_time))
+  combinations_per_second = check_count / diff_time
+
+  print(f'Found: {len(used_combos)} in {diff_time_td}; checked {check_count:,} at {combinations_per_second:,.2f}/s')
 
   return used_combos
 
